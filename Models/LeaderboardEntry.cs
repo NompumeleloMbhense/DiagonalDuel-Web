f0812a983124fc9958b567d1f7a24658a2c7303b
NompumeleloMbhense <nsmbhense6@gmail.com>
2025-06-15 18:52:03 +0200

    Add local leaderboard feature

---
+++ Models/LeaderboardEntry.cs
@@ -0,0 +1,9 @@
+namespace DiagonalDuel_Web.Models
+{
+    public class LeaderboardEntry
+    {
+        public string PlayerName { get; set; } = "";
+        public int Score { get; set; }
+        public DateTime DateAchieved { get; set; }
+    }
+}
